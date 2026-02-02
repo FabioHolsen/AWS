46b0e78548199a4127e6211343d3aa81b3831aff
FabioHolsen <119899295+FabioHolsen@users.noreply.github.com>
2026-02-02 09:16:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+        function conexion(){
+                $host = "basedatos.cluster-ro-cpkgio2u8zmx.us-east-1.rds.amazonaws.com";
+                $user = "admin";
+                $password = "admin123";
+                $database = "alumnos";
+
+        $conexion = mysqli_connect($host,$user,$password,$database);
+        if (!$conexion) {
+                die("Error de conexion: " . mysqli_connect_error());
+        }
+        return $conexion;
+        }
+
+        $conexion = conexion();
+        $result = mysqli_query($conexion, "SELECT * FROM alumnos");
+?>
+
+<HTML>
+<head>
+        <title>Consulta alumnos</title>
+</head>
+<body>
+        <h1>Alumnos</h1>
+        <table border="1">
+        <tr>
+                <th>Nombre</th>
+                <th>Apellidos</th>
+                <th>Clase</th>
+                <th>Correo electronico</th>
+        </tr>
+<?php while ($row = mysqli_fetch_assoc($result)): ?>
+        <tr>
+                <td><?= htmlspecialchars($row['nombre']) ?></td>
+                <td><?= htmlspecialchars($row['apellido1'] . " " . $row['apellido2']) ?></td>
+                <td><?= htmlspecialchars($row['clase']) ?></td>
+                <td><?= htmlspecialchars($row['email']) ?></td>
+
+        </tr>
+<?php endwhile; ?>
+</table>
+</body>
+</HTML>
